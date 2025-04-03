7d7096e686391532de3c45989338dc147acb94c7
Elora Tonaki <113391411+etonaki07@users.noreply.github.com>
2025-04-03 11:24:26 -1000

    Add files via upload

---
+++ Homework 3/test_analyze_data.py
@@ -0,0 +1,10 @@
+from analyze_data import net_profit
+import pytest
+
+def test_net_profit():
+    # TODO: write unit tests for net profit function 
+    pass
+
+# TODO: write unit tests for second function in analyze_data.py
+
+# TODO: write unit tests for second function in analyze_data.py
