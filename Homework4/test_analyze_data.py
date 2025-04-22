f65d1ca0c234c93a0b3072aef52244b891b6f76e
Elora Tonaki <113391411+etonaki07@users.noreply.github.com>
2025-04-21 23:54:47 -1000

    Update test_analyze_data.py

--- Homework4/test_analyze_data.py
+++ Homework4/test_analyze_data.py
@@ -1,10 +1,30 @@
-from analyze_data import net_profit
 import pytest
+from app import app
 
-def test_net_profit():
-    # TODO: write unit tests for net profit function 
-    pass
+@pytest.fixture
+def client():
+    """
+    Fixture to set up the Flask test client.
+    """
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
-# TODO: write unit tests for second function in analyze_data.py
+# TODO: write a test for the entire dataset route
+def test_get_all_movies(client):
 
-# TODO: write unit tests for second function in analyze_data.py
+response = client.get('/movies')
+    assert response.status_code == 200
+    assert isinstance(response.json, list)
+    assert len(response.json) > 0
+    assert all('id' in movie for movie in response.json)
+    assert all('title' in movie for movie in response.json)
+
+    
+# TODO: write a test for the movies between a certain release year range route
+
+# TODO: write a test for the movie by id route
+
+# TODO: write a test for the genres by movie route
+
+# TODO: write a test for the movie by title route
